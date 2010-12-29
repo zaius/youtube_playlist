2e6a4570876fa5eff4e133c87acbd7ff9405d508
David Kelso <david@kelso.id.au>
2010-12-28 17:02:39 -0800

    Add bookmarklet code

---
+++ bookmarklet.js
@@ -0,0 +1,8 @@
+(function() {
+  var url = "//github.com/zaius/Youtube-Playlist/raw/master/youtube_playlist.min.js"
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  var head = document.getElementsByTagName('head')[0];
+  head.appendChild(script);
+})();
