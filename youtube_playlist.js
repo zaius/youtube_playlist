cb93a998094e0df5824ed91815623e11e5ab5814
David Kelso <david@kelso.id.au>
2012-11-24 03:46:06 -0800

    Use iframe embed.

--- youtube_playlist.js
+++ youtube_playlist.js
@@ -3,6 +3,26 @@
     console = { log: function() { } };
   }
 
+  var css = {
+    shadow: {
+      'background-color': 'black',
+      'position': 'fixed',
+      'left': 0, 'top': 0,
+      'width': '100%', 'height': '100%',
+      'z-index': 1000,
+      'opacity': 0.8
+    },
+    player_box: {
+        'position': 'fixed',
+        'left': '50%', 'top': '50%',
+        'width': 640, 'height': 480,
+        'margin-left': -320, 'margin-top': -240,
+        'z-index': 1001
+    },
+    prev_button: { 'float': 'left' },
+    next_button: { 'float': 'right' },
+  }
+
   // courtesy of jquery
   var getScript = function(url, success) {
     var script = document.createElement('script');
@@ -25,131 +45,141 @@
 
 
   var add_player = function($) {
-    $.getScript('https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js', function() {
-      // Video list
-      var ids = [];
-      var url = new RegExp('https?://(www.)?youtube.com/');
-      $('a[href^="http"]').filter(function() {
-        return $(this).attr('href').match(url);
-      }).each(function() {
-        var id = this.href.
-          replace(/^.*v=/, '').
-          replace(/\&.*$/, '');
+    $.getScript('//www.youtube.com/iframe_api');
 
-        // Don't allow duplicates
-        if ($.inArray(id, ids) < 0) { ids.push(id); }
-      });
-      console.log("video ids", ids);
+    // Video list
+    var ids = [];
+    var url = new RegExp('https?://(www.)?youtube.com/');
+    $('a[href^="http"]').filter(function() {
+      return $(this).attr('href').match(url);
+    }).each(function() {
+      var id = this.href.
+        replace(/^.*v=/, '').
+        replace(/\&.*$/, '');
 
-
-      // The index of the currently playing video
-      var index = 0;
-      var index_inc = function() {
-        index++;
-        if (index >= ids.length) { index -= ids.length; }
-      };
-      var index_dec = function() {
-        index--;
-        if (index < 0) { index += ids.length; }
-      };
-
-
-      // Keyboard handlers
-      var cancel = function() {
-        $('#shadow, #player_box').remove();
-        $(document).unbind('keyup.player');
-      };
-
-      $(document).bind('keyup.player', function(e) {
-        if (e.keyCode == 27) {
-          cancel();
-        }
-      });
-
-
-      // The youtube player
-      var player = null;
-      var player_id = 'playlist_player';
-
-      var play_current = function() {
-        console.log('Playing: ', index, ids[index]);
-        player.loadVideoById(ids[index]);
-      };
-
-
-      // Create the UI
-      var $shadow = $('<div />', {
-        id: 'shadow',
-        css: {
-          'background-color': 'black',
-          'position': 'fixed',
-          'left': 0, 'top': 0,
-          'width': '100%', 'height': '100%',
-          'z-index': 1000
-        },
-        click: cancel
-      });
-
-      var $player_box = $('<div />', {
-        id: 'player_box',
-        css: {
-          'position': 'fixed',
-          'left': '50%', 'top': '50%',
-          'width': 320, 'height': 240,
-          'margin-left': -160, 'margin-top': -120,
-          'z-index': 1001
-        }
-      });
-      var $player = $('<div />', { id: player_id });
-      var $prev_button = $('<a />', {
-        href: 'javascript:;',
-        text: 'previous',
-        css: { 'float': 'left' },
-        click: function() {
-          index_dec(); play_current();
-        }
-      });
-      var $next_button = $('<a />', {
-        href: 'javascript:;',
-        text: 'next',
-        css: { 'float': 'right' },
-        click: function() {
-          index_inc(); play_current();
-        }
-      });
-      $player_box.append($player).append($prev_button).append($next_button);
-      $('body').append($shadow).append($player_box);
-      $shadow.css('opacity', 0.8);
-
-
-      // Create the youtube flash player
-      var params = { allowScriptAccess: "always" };
-      var url = "//www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=" + player_id;
-      swfobject.embedSWF(url, player_id, '320', '240', "9.0.0", null, null, params, {'id': player_id});
-
-
-      // Youtube callback functions have to be global
-      window.onYouTubePlayerReady = function(player_id) {
-        player = document.getElementById(player_id);
-        player.addEventListener('onStateChange', 'playerStateChanged');
-        player.addEventListener('onError', 'playerOnError');
-        play_current();
-      };
-      window.playerOnError = function(code) {
-        console.log('error', code);
-        // Remove the offending video
-        ids.splice(index, 1);
-        if (index >= ids.length) { index = 0; };
-        play_current();
-      };
-      window.playerStateChanged = function(state) {
-        // State code 0 means playback ended
-        if (state == 0) {
-          index_inc();
-          play_current();
-        }
-      };
+      // Don't allow duplicates
+      if ($.inArray(id, ids) < 0) { ids.push(id); }
     });
+    console.log("video ids", ids);
+
+
+    // The index of the currently playing video
+    var index = 0;
+
+
+    // The youtube player
+    var player = null;
+    var player_id = 'playlist_player';
+
+    var play_current = function() {
+      console.log('Playing: ', index, ids[index]);
+      player.loadVideoById(ids[index]);
+    };
+    var play_next = function() {
+      index++;
+      if (index >= ids.length) { index -= ids.length; }
+      play_current();
+    };
+    var play_prev = function() {
+      index--;
+      if (index < 0) { index += ids.length; }
+      play_current();
+    };
+
+
+    // Keyboard handlers
+    var cancel = function() {
+      $('#shadow, #player_box').remove();
+      $(document).unbind('keyup.player');
+    };
+
+    $(document).bind('keyup.player', function(e) {
+      if (e.keyCode == 27) { // Escape
+        cancel();
+      } else if (e.keyCode == 39) { // Right
+        play_next();
+      } else if (e.keyCode == 37) { // Left
+        play_prev();
+      }
+    });
+
+    // Create the UI
+    var $shadow = $('<div />', {
+      id: 'shadow',
+      css: css.shadow,
+      click: cancel
+    });
+
+    var $player_box = $('<div />', {
+      id: 'player_box',
+      css: css.player_box
+    });
+
+    var $player = $('<div />', { id: player_id });
+
+    var $prev_button = $('<a />', {
+      href: 'javascript:;',
+      text: 'previous',
+      css: css.prev_button,
+      click: play_prev
+    });
+
+    var $next_button = $('<a />', {
+      href: 'javascript:;',
+      text: 'next',
+      css: css.next_button,
+      click: play_next
+    });
+
+    $player_box.append($player).append($prev_button).append($next_button);
+    $('body').append($shadow).append($player_box);
+
+
+
+    // Player events
+    var player_ready = function(event) {
+      console.log('player ready');
+      event.target.playVideo();
+    };
+
+    var player_error = function(event) {
+      errors = {
+        2: 'invalid video id',
+        5: 'video not supported in html5',
+        100: 'video removed or private',
+        101: 'video not embedable',
+        150: 'video not embedable'
+      }
+      msg = errors[event.data] || 'unknown error';
+      console.log("Error: " + msg);
+
+      // Remove the offending video
+      ids.splice(index, 1);
+      if (index >= ids.length) { index = 0; };
+      play_current();
+    };
+
+    var player_state_change = function(event) {
+      if (event.data == YT.PlayerState.ENDED) {
+        play_next();
+      }
+    };
+
+
+    // Create the youtube player
+    window.onYouTubeIframeAPIReady = function() {
+      console.log('onYouTubeIframeAPIReady');
+      player = new YT.Player(player_id, {
+        height: '390',
+        width: '640',
+        videoId: ids[0],
+        events: {
+          'onReady': player_ready,
+          'onError': player_error,
+          'onStateChange': player_state_change
+        }
+      });
+    };
   };
 
 
